1391db674e18c88042e248438853c161b70802ca
Vanessa Wiyono <13521151@std.stei.itb.ac.id>
2023-04-19 08:07:47 +0000

    styling

--- lib/src/catatan_jadwal/presentation/ubah_catatan_jadwal_screen.py
+++ lib/src/catatan_jadwal/presentation/ubah_catatan_jadwal_screen.py
@@ -10,8 +10,146 @@ from lib.src.utilities.util import image_to_blob
 import lib.src.catatan_jadwal.presentation.catatan_jadwal_widget as catatan_jadwal_widget
 import lib.src.catatan_jadwal.data.catatan_jadwal_model as catatan_jadwal_model
 
+class CalendarButton2(ft.Container):
+    def ubah_jadwal_button_on_click(self, e):
+        self.page.controls.clear()
+        id_catatan_jadwal = 0
+        self.page.update()
+
+    def __init__(self, page: ft.Page):
+        super().__init__(
+            image_src="assets/icons/jadwal_button2.png",
+            on_click=self.ubah_jadwal_button_on_click,
+            width=380,
+            height=64,
+            margin=17
+        )
+        self.page = page
+
+class Agenda2(ft.Container):
+    def __init__(self):
+        super().__init__(
+            content=ft.Text(
+                value="Agenda",
+                size=45,
+                color="#FFFFFF",
+                font_family="Inter Medium",
+                opacity=1
+            ),
+            opacity=1,
+            alignment=ft.alignment.top_left,
+            bgcolor=ft.colors.TRANSPARENT,
+            margin=ft.margin.only(left=81, bottom=22, top=52),
+        )
+
+class Reminders2(ft.UserControl):
+    def __init__(self, catatan_jadwal: catatan_jadwal_model.CatatanJadwal):
+        super().__init__()
+        self._catatan_jadwal = catatan_jadwal
+
+
+    def build(self):
+        _catatan_jadwal_controller = catatan_jadwal_controller.CatatanJadwalController()
+
+        self.__nama_acara = self._catatan_jadwal.getNamaAcara()
+        self.__desc_acara = self._catatan_jadwal.getDescAcara()
+        # self.__desc_acara = self.getDescAcara() 
+        retVal = self._catatan_jadwal.getNamaAcara() + "\n     " + self._catatan_jadwal.getDescAcara()
+        return ft.Column(
+            controls=[
+                ft.Container(
+                    content=ft.Text(
+                        value="Judul Acara: ",
+                        size=19,
+                        color="#FFFFFF",
+                        text_align=ft.TextAlign.LEFT,
+                    ),
+                    width="800",
+                    height="85",
+                    bgcolor="#444980",
+                    border_radius=ft.border_radius.all(10),
+                    padding=ft.padding.only(top=15, left=25, bottom=0, right=0),
+                    margin=ft.margin.only(bottom=22, right=70, left=0),
+                ),
+                ft.Container(
+                    content=ft.Text(
+                        value="Deskripsi Acara: ",
+                        size=19,
+                        color="#FFFFFF",
+                        text_align=ft.TextAlign.LEFT,
+                    ),
+                    width="800",
+                    height="185",
+                    bgcolor="#444980",
+                    border_radius=ft.border_radius.all(10),
+                    padding=ft.padding.only(top=15, left=25, bottom=0, right=0),
+                    margin=ft.margin.only(bottom=22, right=70, left=0),
+                ),
+
+            ],
+            expand=True,
+            scroll=ft.ScrollMode.HIDDEN,
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            horizontal_alignment=ft.CrossAxisAlignment.START,
+        )
+
+class Notification2(ft.Container):
+    def __init__(self, catatan_jadwal: catatan_jadwal_model.CatatanJadwal):
+        self.agenda = Agenda2()
+        self.reminders = Reminders2(catatan_jadwal)
+        super().__init__(
+            content=ft.Container(
+                content=ft.Column(
+                    controls=[
+                        self.agenda,
+                        ft.Divider(
+                            height=1,
+                            color=ft.colors.TRANSPARENT,
+                            visible=False
+                        ),
+                        self.reminders
+                    ],
+                    opacity=1,
+                ),
+                gradient=ft.LinearGradient(
+                    begin=ft.alignment.top_right,
+                    end=ft.alignment.bottom_left,
+                    colors=[
+                        "#e9ebf9",
+                        "#f1f1f1"]
+                ),
+                border_radius=ft.border_radius.all(0),
+                bgcolor=ft.colors.TRANSPARENT
+            ),
+            width=555,
+            padding=ft.padding.only(top=0, left=155, bottom=0, right=0),
+            margin=ft.margin.only(left = 10),
+            alignment=ft.alignment.center,
+        )
+
 # TODO : Ubah Cover
 def main(page: ft.Page):
+
+    page.title = "Memoir - UbahCatatan Jadwal"
+
+    page.window_width = 1440
+    page.window_height = 800
+    page.window_resizable = False
+    page.window_maximizable = False
+
+    page.padding = ft.padding.all(0)
+    page.margin = ft.margin.all(0)
+    page.bgcolor = ft.colors.TRANSPARENT
+
+    page.fonts = {
+        "Inter": "fonts/Inter-Regular.ttf",
+        "Inter SemiBold": "fonts/Inter-SemiBold.otf",
+        "Inter Light": "fonts/Inter-Light-BETA.otf",
+        "Inter Medium": "fonts/Inter-Medium.otf",
+    }
+
+    page.theme = ft.Theme(font_family="Inter")
+
     page.title = "Memoir - Ubah Catatan Jadwal"
 
     page.fonts = {
@@ -132,12 +270,73 @@ def main(page: ft.Page):
             ),
     )
 
-    def batal_ubah_catatan_kegiatan(e):
+    delete_dialog = ft.AlertDialog(
+        modal=True,
+        open=False,
+        actions_alignment=ft.MainAxisAlignment.CENTER,
+        title=ft.Text(
+            value="Apakah kamu yakin ingin menghapus?",
+            size=30,
+            font_family="Inter SemiBold",
+            text_align=ft.TextAlign.CENTER, ),
+        content=ft.Text(
+            value="Catatan Jadwal yang sudah dihapus tidak dapat dikembalikan.",
+            size=20,
+            font_family="Inter",
+            text_align=ft.TextAlign.CENTER,
+        ),
+        actions=[
+            ft.TextButton(
+                content=ft.Text(
+                    value="Ya",
+                    size=25,
+                    font_family="Inter SemiBold",
+                    color=ft.colors.WHITE,
+                ),
+                width=150,
+                height=60,
+                style=ft.ButtonStyle(
+                    color=ft.colors.WHITE,
+                    bgcolor="#e993bd",
+                    shape={
+                        ft.MaterialState.DEFAULT: ft.RoundedRectangleBorder(radius=10)
+                    }
+                ),
+                on_click=back_to_catatan_jadwal_screen,
+            ),
+
+            ft.TextButton(
+                content=ft.Text(
+                    value="Tidak",
+                    size=25,
+                    font_family="Inter SemiBold",
+                    color=ft.colors.WHITE,
+                ),
+                width=150,
+                height=60,
+                style=ft.ButtonStyle(
+                    color=ft.colors.WHITE,
+                    bgcolor="#c5cbe8",
+                    shape={
+                        ft.MaterialState.DEFAULT: ft.RoundedRectangleBorder(radius=10)
+                    },
+                ),
+                on_click=close_dialog,
+            ),
+        ],
+    )
+
+    def hapus_catatan_jadwal(e):
+        page.dialog = delete_dialog
+        delete_dialog.open = True
+        page.update()
+
+    def batal_ubah_catatan_jadwal(e):
         page.dialog = cancel_dialog
         page.dialog.open = True
         page.update()
 
-    def ubah_catatan_kegiatan(e):
+    def ubah_catatan_jadwal(e):
         try:
             if desc_acara.value == "" or nama_acara.value == "" or nama_acara.value != "" or desc_acara.value != "":
                 page.snack_bar.content.value = "Mohon isi semua kolom"
@@ -145,12 +344,12 @@ def main(page: ft.Page):
                 page.update()
                 return
 
-            page.snack_bar.content.value = "Catatan Kegiatan berhasil diubah"
+            page.snack_bar.content.value = "Catatan Jadwal berhasil diubah"
             page.snack_bar.open = True
             page.update()
 
         except Exception as e:
-            page.snack_bar.content.value = "Catatan Kegiatan gagal diubah"
+            page.snack_bar.content.value = "Catatan Jadwal gagal diubah"
             page.snack_bar.open = True
             page.update()
             print(e)
@@ -160,9 +359,9 @@ def main(page: ft.Page):
         page.update()
 
     calendar = catatan_jadwal_widget.CalendarLeft()
-    calendar_button = catatan_jadwal_widget.CalendarButton(page)
+    calendar_button = CalendarButton2(page)
     tes = catatan_jadwal_model.CatatanJadwal(1, 2, "aa", "ACARA XXX XXX XXX", "Agenda", "01:02:03")
-    notification = catatan_jadwal_widget.Notification(tes)
+    notification = Notification2(tes)
     home_button = catatan_jadwal_widget.HomeButton(page)
 
     left_column = ft.Container(
