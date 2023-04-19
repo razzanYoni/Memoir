b469400ec505904a065be6c6be29f9d2fb052d65
Vanessa Wiyono <13521151@std.stei.itb.ac.id>
2023-04-19 04:35:04 +0000

    resolve hardcode

--- lib/src/catatan_jadwal/presentation/catatan_jadwal_widget.py
+++ lib/src/catatan_jadwal/presentation/catatan_jadwal_widget.py
@@ -5,8 +5,30 @@ import lib.src.artikel.presentation.artikel_screen as artikel_screen
 import lib.src.catatan_jadwal.presentation.catatan_jadwal_screen as catatan_jadwal_screen
 import lib.src.catatan_target.presentation.catatan_target_screen as catatan_target_screen
 import lib.src.catatan_kegiatan.presentation.catatan_kegiatan_screen as catatan_kegiatan_screen
-
+import lib.src.catatan_jadwal.data.catatan_jadwal_model as catatan_jadwal_model
 import lib.src.catatan_jadwal.data.catatan_jadwal_repo as catatan_jadwal_repo
+import lib.src.catatan_jadwal.presentation.ubah_catatan_jadwal_screen as ubah_catatan_jadwal_screen
+import lib.src.catatan_jadwal.controller.catatan_jadwal_controller as catatan_jadwal_controller
+import lib.home_page.main_screen as main_screen
+
+class HomeButton(ft.Container):
+    def __init__(self, page: ft.Page):
+        super().__init__(
+            image_src="assets/icons/home_button_putih.png",
+            on_click=self.home_button_on_click,
+            width=64,
+            height=64,
+            padding=ft.padding.only(top=0, left=0, bottom=0, right=0),
+            margin=ft.margin.only(bottom = 600, left=70),
+        )
+        self.page = page
+
+    def home_button_on_click(self, e):
+        self.page.controls.clear()
+        main_screen.main(self.page)
+        self.page.update()
+
+        
 
 class CalendarLeft(ft.Container):
     def __init__(self):
@@ -30,7 +52,7 @@ class CalendarLeft(ft.Container):
                 ),
                 border_radius=ft.border_radius.all(0),
                 bgcolor="A7AECD",
-                padding=ft.padding.only(top=50, left=72, bottom=30, right=72),
+                padding=ft.padding.only(top=50, left=32, bottom=30, right=72),
             ),
             width=825,
             height=625,
@@ -45,7 +67,7 @@ class CalendarDate(ft.Container):
 
     def build(self):
         # Define weekday names
-        weekday_names = ["Mon  ", " Tue  ", "Wed  ", " Thu  ", "  Fri  ", "  Sat  ", "  Sun  "]
+        weekday_names = [" Mon  ", "  Tue  ", " Wed ", "  Thu  ", "   Fri  ", "   Sat  ", "   Sun  "]
 
         # Create weekday name labels
         weekday_labels = [
@@ -92,7 +114,7 @@ class CalendarDate(ft.Container):
                         height=47
                     )
                 )
-
+            
         # Group days into rows of 7
         rows = [weekday_labels] + [calendar_days[i:i+7] for i in range(0, len(calendar_days), 7)]
 
@@ -154,12 +176,22 @@ class Calendar(ft.Container):
 
 
 class CalendarButton(ft.Container):
-    def __init__(self):
+    def __init__(self, page: ft.Page):
         super().__init__(
-            height=50,
-            width=150,
-            bgcolor="#FFFFFF",
+            image_src="assets/icons/jadwal_button1.png",
+            on_click=self.ubah_jadwal_button_on_click,
+            width=380,
+            height=64,
+            margin=17
         )
+        self.page = page
+
+    def ubah_jadwal_button_on_click(self, e):
+        self.page.controls.clear()
+        id_catatan_jadwal = 0
+        ubah_catatan_jadwal_screen.main(self.page, id_catatan_jadwal)
+        self.page.update()
+
 
 class Agenda(ft.Container):
     def __init__(self):
@@ -181,81 +213,175 @@ class Agenda(ft.Container):
 
 
 # TODO: Reminder Control, wait for the backend
+# class Reminders(ft.UserControl):
+#     def __init__(self):
+#         super().__init__(
+#             expand=True,
+#         )
+
+#     def build(self):
+#         return ft.Column(
+#             controls=[
+#                 ft.Container(
+#                     content=ft.Text(
+#                         value="  Agenda\n  ACARA XXX XXX XXX\n   ",
+#                         size=19,
+#                         color="#FFFFFF",
+#                         text_align=ft.TextAlign.LEFT,
+#                     ),
+#                     width="800",
+#                     bgcolor="#444980",
+#                     border_radius=ft.border_radius.all(10),
+#                     padding=ft.padding.only(top=15, left=0, bottom=0, right=0),
+#                     margin=ft.margin.only(bottom=22, right=70, left=0),
+#                 ),
+
+#                 ft.Container(
+#                     content=ft.Text(
+#                         value="  Agenda\n  ACARA XXX XXX XXX\n   ",
+#                         size=19,
+#                         color="#FFFFFF",
+#                         text_align=ft.TextAlign.LEFT,
+#                     ),
+#                     width="800",
+#                     bgcolor="#444980",
+#                     border_radius=ft.border_radius.all(10),
+#                     padding=ft.padding.only(top=15, left=0, bottom=0, right=0),
+#                     margin=ft.margin.only(bottom=22, right=70, left=0),
+#                 ),
+#                 ft.Container(
+#                     content=ft.Text(
+#                         value="  Agenda\n  ACARA XXX XXX XXX\n   ",
+#                         size=19,
+#                         color="#FFFFFF",
+#                         text_align=ft.TextAlign.LEFT,
+#                     ),
+#                     width="800",
+#                     bgcolor="#444980",
+#                     border_radius=ft.border_radius.all(10),
+#                     padding=ft.padding.only(top=15, left=0, bottom=0, right=0),
+#                     margin=ft.margin.only(bottom=22, right=70, left=0),
+#                 ),
+
+#                 ft.Container(
+#                     content=ft.Text(
+#                         value="  Agenda\n  ACARA XXX XXX XXX\n   ",
+#                         size=19,
+#                         color="#FFFFFF",
+#                         text_align=ft.TextAlign.LEFT,
+#                     ),
+#                     width="800",
+#                     bgcolor="#444980",
+#                     border_radius=ft.border_radius.all(10),
+#                     padding=ft.padding.only(top=15, left=0, bottom=0, right=0),
+#                     margin=ft.margin.only(bottom=22, right=70, left=0),
+#                 ),
+
+#                 ft.Container(
+#                     content=ft.Text(
+#                         value="  Agenda\n  ACARA XXX XXX XXX\n   ",
+#                         size=19,
+#                         color="#FFFFFF",
+#                         text_align=ft.TextAlign.LEFT,
+#                     ),
+#                     width="800",
+#                     bgcolor="#444980",
+#                     border_radius=ft.border_radius.all(10),
+#                     padding=ft.padding.only(top=15, left=0, bottom=0, right=0),
+#                     margin=ft.margin.only(bottom=22, right=70, left=0),
+#                 ),
+#             ],
+#             expand=True,
+#             scroll=ft.ScrollMode.HIDDEN,
+#             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+#             horizontal_alignment=ft.CrossAxisAlignment.START,
+#         )
+
 class Reminders(ft.UserControl):
-    def __init__(self):
-        super().__init__(
-            expand=True,
-        )
+    def __init__(self, catatan_jadwal: catatan_jadwal_model.CatatanJadwal):
+        super().__init__()
+        self._catatan_jadwal = catatan_jadwal
 
     def build(self):
+        _catatan_jadwal_controller = catatan_jadwal_controller.CatatanJadwalController()
+
+        self.__nama_acara = self._catatan_jadwal.getNamaAcara()
+        self.__desc_acara = self._catatan_jadwal.getDescAcara()
+        # self.__desc_acara = self.getDescAcara() 
+        retVal = self._catatan_jadwal.getNamaAcara() + "\n     " + self._catatan_jadwal.getDescAcara()
         return ft.Column(
             controls=[
                 ft.Container(
                     content=ft.Text(
-                        value="  Agenda\n  ACARA XXX XXX XXX\n   ",
+                        value=retVal,
                         size=19,
                         color="#FFFFFF",
                         text_align=ft.TextAlign.LEFT,
                     ),
                     width="800",
+                    height="85",
                     bgcolor="#444980",
                     border_radius=ft.border_radius.all(10),
-                    padding=ft.padding.only(top=15, left=0, bottom=0, right=0),
+                    padding=ft.padding.only(top=15, left=25, bottom=0, right=0),
                     margin=ft.margin.only(bottom=22, right=70, left=0),
                 ),
 
                 ft.Container(
                     content=ft.Text(
-                        value="  Agenda\n  ACARA XXX XXX XXX\n   ",
+                        value=retVal,
                         size=19,
                         color="#FFFFFF",
                         text_align=ft.TextAlign.LEFT,
                     ),
                     width="800",
+                    height="85",
                     bgcolor="#444980",
                     border_radius=ft.border_radius.all(10),
-                    padding=ft.padding.only(top=15, left=0, bottom=0, right=0),
+                    padding=ft.padding.only(top=15, left=25, bottom=0, right=0),
                     margin=ft.margin.only(bottom=22, right=70, left=0),
                 ),
                 ft.Container(
                     content=ft.Text(
-                        value="  Agenda\n  ACARA XXX XXX XXX\n   ",
+                        value=retVal,
                         size=19,
                         color="#FFFFFF",
                         text_align=ft.TextAlign.LEFT,
                     ),
                     width="800",
+                    height="85",
                     bgcolor="#444980",
                     border_radius=ft.border_radius.all(10),
-                    padding=ft.padding.only(top=15, left=0, bottom=0, right=0),
-                    margin=ft.margin.only(bottom=22, right=70, left=0),
-                ),
-
-                ft.Container(
-                    content=ft.Text(
-                        value="  Agenda\n  ACARA XXX XXX XXX\n   ",
-                        size=19,
-                        color="#FFFFFF",
-                        text_align=ft.TextAlign.LEFT,
-                    ),
-                    width="800",
-                    bgcolor="#444980",
-                    border_radius=ft.border_radius.all(10),
-                    padding=ft.padding.only(top=15, left=0, bottom=0, right=0),
+                    padding=ft.padding.only(top=15, left=25, bottom=0, right=0),
                     margin=ft.margin.only(bottom=22, right=70, left=0),
                 ),
 
                 ft.Container(
                     content=ft.Text(
-                        value="  Agenda\n  ACARA XXX XXX XXX\n   ",
+                        value=retVal,
                         size=19,
                         color="#FFFFFF",
                         text_align=ft.TextAlign.LEFT,
                     ),
                     width="800",
+                    height="85",
                     bgcolor="#444980",
                     border_radius=ft.border_radius.all(10),
-                    padding=ft.padding.only(top=15, left=0, bottom=0, right=0),
+                    padding=ft.padding.only(top=15, left=25, bottom=0, right=0),
+                    margin=ft.margin.only(bottom=22, right=70, left=0),
+                ),
+
+                ft.Container(
+                    content=ft.Text(
+                        value=retVal,
+                        size=19,
+                        color="#FFFFFF",
+                        text_align=ft.TextAlign.LEFT,
+                    ),
+                    width="800",
+                    height="85",
+                    bgcolor="#444980",
+                    border_radius=ft.border_radius.all(10),
+                    padding=ft.padding.only(top=15, left=25, bottom=0, right=0),
                     margin=ft.margin.only(bottom=22, right=70, left=0),
                 ),
             ],
@@ -265,12 +391,10 @@ class Reminders(ft.UserControl):
             horizontal_alignment=ft.CrossAxisAlignment.START,
         )
 
-
-
 class Notification(ft.Container):
-    def __init__(self):
+    def __init__(self, catatan_jadwal: catatan_jadwal_model.CatatanJadwal):
         self.agenda = Agenda()
-        self.reminders = Reminders()
+        self.reminders = Reminders(catatan_jadwal)
         super().__init__(
             content=ft.Container(
                 content=ft.Column(
@@ -298,5 +422,92 @@ class Notification(ft.Container):
             width=555,
             padding=ft.padding.only(top=0, left=155, bottom=0, right=0),
             margin=ft.margin.only(left = 10),
+            alignment=ft.alignment.center,
+        )
+
+
+# class Notification(ft.Container):
+#     def __init__(self):
+#         self.agenda = Agenda()
+#         self.reminders = Reminders()
+#         super().__init__(
+#             content=ft.Container(
+#                 content=ft.Column(
+#                     controls=[
+#                         self.agenda,
+#                         ft.Divider(
+#                             height=1,
+#                             color=ft.colors.TRANSPARENT,
+#                             visible=False
+#                         ),
+#                         self.reminders
+#                     ],
+#                     opacity=1,
+#                 ),
+#                 gradient=ft.LinearGradient(
+#                     begin=ft.alignment.top_right,
+#                     end=ft.alignment.bottom_left,
+#                     colors=[
+#                         "#e9ebf9",
+#                         "#f1f1f1"]
+#                 ),
+#                 border_radius=ft.border_radius.all(0),
+#                 bgcolor=ft.colors.TRANSPARENT
+#             ),
+#             width=555,
+#             padding=ft.padding.only(top=0, left=155, bottom=0, right=0),
+#             margin=ft.margin.only(left = 10),
+#             alignment=ft.alignment.center,
+#         )
+
+# jadwal
+class CatatanJadwal(ft.UserControl):
+    def __init__(self, id_jadwal, tanggal, durasi, desc_acara, nama_acara, waktu):
+        self.__id_jadwal = id_jadwal
+        self.__tanggal = tanggal
+        self.__waktu = waktu
+        self.__nama_acara = nama_acara
+        self.__desc_acara = desc_acara
+        self.__durasi = durasi
+
+    def catatan_jadwal_on_click(self, e):
+        self.page.controls.clear()
+        # self.page.clean()
+        catatan_jadwal_screen.main(self.page, self._catatan_jadwal.getID())
+
+    def get_nama_acara(self):
+        return self.__nama_acara
+
+
+class DaftarCatatanJadwal(ft.UserControl):
+    def __init__(self, page: ft.Page):
+        super().__init__()
+        self.page = page
+
+    def build(self):
+        _catatan_jadwal_controller = catatan_jadwal_controller.CatatanJadwalController()
+        catatan_jadwal_list = _catatan_jadwal_controller.getListCatatanJadwal()
+
+        if len(catatan_jadwal_list) == 0:
+            return ft.Container(
+                content=ft.Text(
+                    value="Tidak ada catatan jadwal",
+                    size=30,
+                    text_align=ft.TextAlign.CENTER,
+                ),
+                alignment=ft.alignment.center,
+                expand=True,
+                height=300,
+                width=1164,
+            )
+        return ft.Container(
+            content=ft.Column(
+                controls=[
+                    CatatanJadwal(i) for i in catatan_jadwal_list
+                ],
+                spacing=15,
+            ),
+            width=1164,
+            bgcolor=ft.colors.TRANSPARENT,
             alignment=ft.alignment.center,
         )
